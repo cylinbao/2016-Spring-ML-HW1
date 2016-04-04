5c72b1f68557c637f020c059a1d6976ef814508c
Chien Yu Lin <myislin@gmail.com>
2016-04-04 21:38:05 +0800

    fulfill program flow

--- hw1_0450225.m
+++ hw1_0450225.m
@@ -8,3 +8,27 @@ addpath ./test
 prediction_ml;
 test_ml;
 rmpath ./test
+
+addpath ./train
+train_map;
+rmpath ./train
+addpath ./test
+prediction_map;
+test_map;
+rmpath ./test
+
+addpath ./train
+train_ml_cx;
+rmpath ./train
+addpath ./test
+prediction_ml_cx;
+test_ml_cx;
+rmpath ./test
+
+addpath ./train
+train_map_cx;
+rmpath ./train
+addpath ./test
+prediction_map_cx;
+test_map_cx;
+rmpath ./test
